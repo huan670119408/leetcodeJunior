82b55458ab52104454c495ff3646eb7e09a23b0b
libingyi <670119408@qq.com>
2019-05-31 14:42:58 +0800

    'add'

--- src/main/java/sortAndSearch/Solution_2.java
+++ src/main/java/sortAndSearch/Solution_2.java
@@ -20,8 +20,22 @@ package main.java.sortAndSearch;
  */
 public class Solution_2 {
 
-    public int firstBadVersion(int n) {
+    private boolean isBadVersion(int n){
+        return true;
+    }
 
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while (high>low) {
+            int mid = low + (high -low)/2;
+            if(isBadVersion(mid)){
+                high = mid;
+            } else {
+                low = mid +1;
+            }
+        }
+        return low;
     }
 
 }
